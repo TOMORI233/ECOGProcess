7c5edb473940a01a53b881342887adf743b38f97
418-SPR <11815013@zju.edu.cn>
2022-05-31 13:42:51 +0800

    add joinSTDAll,for both correct and wrong trials

--- clickTrainWorkingMemory/clickTrainWMProcess_active.m
+++ clickTrainWorkingMemory/clickTrainWMProcess_active.m
@@ -1,62 +1,64 @@
 addpath(genpath("..\..\ECOGProcess"));
 %% Data loading
 clear; clc; close all;
-BLOCKPATH = 'G:\ECoG\chouchou\cc20220528\Block-1';
+BLOCKPATH = 'G:\ECoG\chouchou\cc20220530\Block-4';
 posIndex = 1; % 1-AC, 2-PFC
-pairStr = {'4-4.06RC','4-4.06RD','4-4.06IC','4-4.06ID','4-4.06InC','4-4.06InD','40-40.6RC','40-40.6RD','Tone-C','Tone-D'};
+
+soundDuration = 200; % ms
+
+% pairStr = {'4-4.06RC','4-4.06RD','4-4.06IC','4-4.06ID','4-4.06InC','4-4.06InD','40-40.6RC','40-40.6RD','Tone-C','Tone-D'};
+pairStr = {'4-4.06RC','4-4.06RD','4-4.06IC','4-4.06ID','Tone-C','Tone-D'};
 posStr = ["LAuC", "LPFC"];
+
+
+
 temp = TDTbin2mat(BLOCKPATH, 'TYPE', {'epocs'});
 epocs = temp.epocs;
-soundDuration = 600; % ms
-% temp = TDTbin2mat(BLOCKPATH, 'TYPE', {'streams'}, 'STORE', posStr(posIndex));
-% streams = temp.streams;
-% fs0 = streams.(posStr(posIndex)).fs;
-% totalChNum = length(streams.(posStr(posIndex)).channels);
+
+temp = TDTbin2mat(BLOCKPATH, 'TYPE', {'streams'}, 'STORE', posStr(posIndex));
+streams = temp.streams;
+
+ECOGDataset = streams.(posStr(posIndex));
+fs0 = ECOGDataset.fs;
+
+AREANAME = ["AC", "PFC"];
+temp = string(split(BLOCKPATH, '\'));
+DateStr = temp(end - 1);
 
 %% Params settings
-window = [-3000, 3000]; % ms
-choiceWin = [-300, 1000]; % ms
+choiceWin = [0, 1000]; % ms
 fs = 300; % Hz, for downsampling
-scaleFactor = 1e6;
 
 %% Behavior processing
-trialAll = ActiveProcess_clickTrainWM(epocs, choiceWin, soundDuration);
+% trialAll = ActiveProcess_clickTrainWM(epocs, choiceWin, soundDuration); % if input soundDuration, means offset choiceWin, otherwise, choiveWin aligns to deviant onset
+trialAll = ActiveProcess_clickTrainWM(epocs, choiceWin);
 trialsNoInterrupt = trialAll([trialAll.interrupt] == false);
 ISI = fix(mean(cellfun(@(x, y) (x(end) - x(1)) / y, {trialsNoInterrupt.soundOnsetSeq}, {trialsNoInterrupt.stdNum})));
 
 %% Plot behavior result
-trials1 = trialsNoInterrupt(1:floor(length(trialsNoInterrupt)/2));
-[Fig, mAxe] = plotClickTrainWMBehaviorOnly(trials1, "k", {'control', 'dev'},pairStr);
-trials2 = trialsNoInterrupt(floor(length(trialsNoInterrupt))/2 + 1:end);
-[Fig, mAxe] = plotClickTrainWMBehaviorOnly(trials2, "k", {'control', 'dev'},pairStr);
+trials = trialsNoInterrupt;
+[Fig, mAxe] = plotClickTrainWMBehaviorOnly(trials, "k", {'control', 'dev'},pairStr);
 
-%% ECOG
-trials = [{trials1_3([trials1_3.correct] == true & [trials1_3.oddballType] == "DEV")}; ...
-          {trials4_6([trials4_6.correct] == true & [trials4_6.oddballType] == "DEV")}; ...
-          {trials7_9([trials7_9.correct] == true & [trials7_9.oddballType] == "DEV")}];
-legendStr = ["1-3", "4-6", "7-9"];
+diffPairs = [[trials.stdOrdr]' [trials.devOrdr]'];
+diffPairsUnique = unique(diffPairs, 'rows');
+stdType = unique(diffPairsUnique(:,1));
 
-for tIndex = 1:length(trials)
-    result = cellfun(@(x) x * scaleFactor, selectEcog(streams.(posStr(posIndex)), trials{tIndex}, "dev onset", window), "UniformOutput", false);
-    temp = cell2mat(result);
-    chMean = zeros(totalChNum, size(result{1}, 2));
-    chSE = zeros(totalChNum, size(result{1}, 2));
-    
-    for index = 1:totalChNum
-        chMean(index, :) = mean(temp(index:totalChNum:length(result) * totalChNum, :), 1);
-        chSE(index, :) = std(temp(index:totalChNum:length(result) * totalChNum, :), [], 1);
-    end
-
-    %% Raw wave
-    Fig1(tIndex) = plotRawWave(chMean, chSE, window);
-    plotLayout(Fig1(tIndex), posIndex);
-    set(Fig1(tIndex), "NumberTitle", "off", "Name", legendStr(tIndex));
-    
-    %% Time-Freq
-    Fig2(tIndex) = plotTimeFreqAnalysis(double(chMean), fs0, fs);
-    plotLayout(Fig2(tIndex), posIndex);
-    set(Fig2(tIndex), "NumberTitle", "off", "Name", legendStr(tIndex));
+%% Prediction
+window = [-2500, 6000]; % ms
+for sIndex = 1 : length(stdType)
+trialP = trialAll([trialAll.stdOrdr] == stdType(sIndex) & [trialAll.interrupt] == false);
+[chMean2, chStd2] = joinSTDAll(trialP, ECOGDataset, window);
+FigP(1) = plotRawWave(chMean, chStd, window);
+drawnow;
+FigP(2) = plotTimeFreqAnalysis(double(chMean), fs0, fs, window);
+drawnow;
 end
+% window = [-2500, 6000]; % ms
+% [chMean, chStd] = joinSTD(trialAll, ECOGDataset, window);
+% FigP(1) = plotRawWave(chMean, chStd, window);
+% drawnow;
+% FigP(2) = plotTimeFreqAnalysis(double(chMean), fs0, fs, window);
+% drawnow;
 
 % Scale
 scaleAxes(Fig1, "x", [-500, 1000]);
