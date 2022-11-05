e2874af65117237adae113016b2087f385ef3427
TOMORI233 <ihatewo@qq.com>
2022-11-05 17:16:01 +0800

    update 20221105 by xhx

--- utils/exportData_xhx.m
+++ utils/exportData_xhx.m
@@ -1,62 +1,61 @@
 clear; clc; close all;
 
 %% 7-10Freq active
-disp("Exporting 7-10Freq active...");
-tb = readtable("D:\Education\Lab\Projects\ECOG\MAT Data\PEOdd7-10.xlsx");
-BLOCKROOTPATH = "E:\ECoG\TDT Data";
+TBPATH = "D:\Education\Lab\Projects\ECOG\MAT Data\PEOdd7-10.xlsx"; % table path
+BLOCKROOTPATH = "E:\ECoG\TDT Data"; % TDT Block path
+SAVEROOTPATH = "D:\Education\Lab\Projects\ECOG\MAT Data"; % MAT path to save
+PROTOCOL = "7-10Freq Active";
 
+tb = readtable(TBPATH);
 Monkeys = string(tb.Monkey);
 Dates = string(num2str(tb.Date));
 Blocks = string(num2str(tb.ActiveBlock));
+Exported = tb.Exported;
 
 BLOCKPATHs = arrayfun(@(x, y, z) char(fullfile(BLOCKROOTPATH, x, strcat(x, y), strcat("Block-", z))), Monkeys, Dates, Blocks, "UniformOutput", false);
-SAVEPATHs = arrayfun(@(x) char(fullfile("D:\Education\Lab\Projects\ECOG\MAT Data", upper(x), "7-10Freq Active\")), Monkeys, "UniformOutput", false);
+SAVEPATHs = arrayfun(@(x) char(fullfile(SAVEROOTPATH, upper(x), PROTOCOL, "\")), Monkeys, "UniformOutput", false);
 
 params.choiceWin = [100, 600];
 params.processFcn = @ActiveProcess_7_10Freq;
-exportDataFcn(BLOCKPATHs, SAVEPATHs, params, 24);
+tb.Exported = exportDataFcn(BLOCKPATHs, SAVEPATHs, params, Exported);
+writetable(tb, TBPATH);
 
 %% Fcn
-function exportDataFcn(BLOCKPATHs, SAVEPATHs, params, startIdx, endIdx)
-    narginchk(3, 5);
-
-    if nargin < 4
-        startIdx = 1;
-    end
-
-    if nargin < 5
-        endIdx = length(BLOCKPATHs);
-    end
-
+function Exported = exportDataFcn(BLOCKPATHs, SAVEPATHs, params, Exported)
     fd = 500; % Hz
 
-    for index = startIdx:endIdx
+    idxAll = find(~Exported);
+
+    for index = 1:length(idxAll)
         AREANAME = ["AC", "PFC"];
-        temp = string(split(BLOCKPATHs{index}, '\'));
+        temp = string(split(BLOCKPATHs{idxAll(index)}, '\'));
         DateStr = temp(end - 1);
-        mkdir(fullfile(SAVEPATHs{index}, DateStr));
+        mkdir(fullfile(SAVEPATHs{idxAll(index)}, DateStr));
         
         % AC
         disp("Loading AC Data...");
         params.posIndex = 1;
         tic
-        [trialAll, ECOGDataset] = ECOGPreprocess(BLOCKPATHs{index}, params);
+        [trialAll, ECOGDataset] = ECOGPreprocess(BLOCKPATHs{idxAll(index)}, params);
         ECOGDataset = ECOGDownsample(ECOGDataset, fd);
         ECOGDataset = ECOGFilter(ECOGDataset);
         disp("Saving...");
-        save(strcat(SAVEPATHs{index}, DateStr, "\", DateStr, "_", AREANAME(params.posIndex), ".mat"), "ECOGDataset", "trialAll", "-mat", "-v7.3");
+        save(strcat(SAVEPATHs{idxAll(index)}, DateStr, "\", DateStr, "_", AREANAME(params.posIndex), ".mat"), "ECOGDataset", "trialAll", "-mat", "-v7.3");
         toc
         
         % PFC
         disp("Loading PFC Data...");
         params.posIndex = 2;
         tic
-        [~, ECOGDataset] = ECOGPreprocess(BLOCKPATHs{index}, params);
+        [~, ECOGDataset] = ECOGPreprocess(BLOCKPATHs{idxAll(index)}, params);
         ECOGDataset = ECOGDownsample(ECOGDataset, fd);
         ECOGDataset = ECOGFilter(ECOGDataset);
         disp("Saving...");
-        save(strcat(SAVEPATHs{index}, DateStr, "\", DateStr, "_", AREANAME(params.posIndex), ".mat"), "ECOGDataset", "trialAll", "-mat", "-v7.3");
+        save(strcat(SAVEPATHs{idxAll(index)}, DateStr, "\", DateStr, "_", AREANAME(params.posIndex), ".mat"), "ECOGDataset", "trialAll", "-mat", "-v7.3");
         toc
+
+        Exported(idxAll(index)) = true;
     end
 
+    return;
 end
