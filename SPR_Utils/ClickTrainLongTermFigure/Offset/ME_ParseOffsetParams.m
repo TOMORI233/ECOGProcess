7d4bce57e2255c21191e146d075317c5bc48fe69
418-SPR <11815013@zju.edu.cn>
2022-12-11 17:14:27 +0800

    jpg2png

--- SPR_Utils/ClickTrainLongTermFigure/Offset/ME_ParseOffsetParams.m
+++ SPR_Utils/ClickTrainLongTermFigure/Offset/ME_ParseOffsetParams.m
@@ -1,23 +1,38 @@
-function MSTIParams = ME_ParseMSTIParams(protStr)
-
-ME_MSTI_Update_Config;
+function CTLParams = ME_Species_ParseCTLParams(protStr)
+% parse monkey ecog species click train longterm parameters
 
 % load excel
-configPath = strcat(fileparts(fileparts(mfilename("fullpath"))), "\MSTI\ME_MSTIConfig.xlsx");
+configPath = strcat(fileparts(mfilename("fullpath")), "\Species_CTLConfig.xlsx");
 configTable = table2struct(readtable(configPath));
 mProtocol = configTable(matches({configTable.paradigm}', protStr));
 
 % parse CTLProt
-MSTIParams.Window = str2double(string(strsplit(mProtocol.Window, ",")));
-MSTIParams.DevPlotWin = str2double(string(strsplit(mProtocol.DevPlotWin, ",")));
-MSTIParams.stimStrs = string(strsplit(mProtocol.trialTypes, ","));
-MSTIParams.protStr = string(mProtocol.paradigm);
-MSTIParams.orderIndex = cell2mat(cellfun(@(x) str2double(string(strsplit(x, "-"))), strsplit(string(strsplit(mProtocol.orderIndex, ";")), ",")', "uni", false));
-MSTIParams.Std_Dev_Onset = cell2mat(cellfun(@(x) str2double(strsplit(x, ",")), string(strsplit(mProtocol.Std_Dev_Onset, ";")), "uni", false)');
-MSTIParams.DevOnset = MSTIParams.Std_Dev_Onset(:, end);
-MSTIParams.S1_S2 = table2array(cell2table(cellfun(@(x) string(strsplit(x, ",")), string(strsplit(mProtocol.S1_S2, ";")), "uni", false)'));
-MSTIParams.colors = string(strsplit(mProtocol.colors, ","));
-MSTIParams.fhp = mProtocol.fhp;
-MSTIParams.flp = mProtocol.flp;
+CTLParams.protStr = string(mProtocol.paradigm);
+CTLParams.S1Duration = str2double(string(strsplit(mProtocol.S1Duration, ",")));
+CTLParams.Window = str2double(string(strsplit(mProtocol.Window, ",")));
+CTLParams.Offset = str2double(string(strsplit(mProtocol.Offset, ",")));
+CTLParams.stimStrs = strrep(string(strsplit(mProtocol.trialTypes, ",")), "_", "-");
+CTLParams.FFTWin = str2double(string(strsplit(mProtocol.FFTWin, ",")));
+CTLParams.ICAWin = str2double(string(strsplit(mProtocol.ICAWin, ",")));
+CTLParams.BaseICI = str2double(string(strsplit(mProtocol.BaseICI, ",")));
+CTLParams.ICI2 = str2double(string(strsplit(mProtocol.ICI2, ",")));
+CTLParams.quantWin = str2double(string(strsplit(mProtocol.quantWin, ",")));
+CTLParams.sponWin = str2double(string(strsplit(mProtocol.sponWin, ",")));
+CTLParams.latencyWin = str2double(string(strsplit(mProtocol.latencyWin, ",")));
+CTLParams.fhp = mProtocol.fhp;
+CTLParams.flp = mProtocol.flp;
+CTLParams.CRIMethod = mProtocol.CRIMethod;
+CTLParams.pBase = mProtocol.pBase;
+CTLParams.CRIScale = reshape(str2double(string(strsplit(mProtocol.CRIScale, ","))), 2, 2);
+CTLParams.yScale = str2double(string(strsplit(mProtocol.yScale, ",")));
+
+group_Index =  string(strsplit(mProtocol.group_Index, ";"));
+for cIndex = 1 : length(group_Index) 
+    CTLParams.group_Index{cIndex, 1} = str2double(strsplit(group_Index(cIndex), ","));
+end
+CTLParams.group_Str = strrep(string(strsplit(mProtocol.group_Str, ",")), "_", "-");
+CTLParams.colors = string(strsplit(mProtocol.colors, ","));
+
+
 end
 
