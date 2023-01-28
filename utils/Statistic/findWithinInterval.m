fd8639dacb5cc516e097421332a8443fe934150e
spring <11815013@zju.edu.cn>
2023-01-28 17:08:36 +0800

    spr20230128

--- utils/Statistic/findWithinInterval.m
+++ utils/Statistic/findWithinInterval.m
@@ -1,4 +1,8 @@
-function [resVal,idx] = findWithinInterval(value, range)
-    idx = find(value>=range(1) & value<=range(2));
-    resVal = value(idx);
+function [resVal,idx] = findWithinInterval(value, range, col)
+narginchk(2,3);
+if nargin < 3
+    col = 1;
+end
+idx = find(value(:, col)>=range(1) & value(:, col)<=range(2));
+resVal = value(idx, :);
 end
