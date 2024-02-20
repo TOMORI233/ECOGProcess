6ab7177b61abaa78b02b8a2552656b800164e062
TOMORI233 <ihatewo@qq.com>
2024-02-20 13:00:02 +0800

    update 20240220 by xhx

--- plot/plotRawWaveMulti.m
+++ plot/plotRawWaveMulti.m
@@ -80,6 +80,7 @@ function Fig = plotRawWaveMulti(chData, window, varargin)
                 chErr = getOr(chData(index), "chErr");
 
                 color = getOr(chData(index), "color", "r");
+                color = validatecolor(color);
                 hsi = rgb2hsv(color);
                 if hsi(2) == 0 % gray or black
                     hsi(3) = min([1.1 * hsi(3), 0.9]);
@@ -148,7 +149,7 @@ function Fig = plotRawWaveMulti(chData, window, varargin)
 
     end
 
-    scaleAxes(Fig, "y", "on", "symOpt", "max");
+    scaleAxes(Fig, "y", "on");
 
     return;
 end
