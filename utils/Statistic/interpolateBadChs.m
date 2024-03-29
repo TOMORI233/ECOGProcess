2a4dd678a9d01f228eb619f7fbd239cc7ecaf968
TOMORI233 <ihatewo@qq.com>
2024-03-29 17:59:03 +0800

    update 2024-03-29 17:59:03 by tomori

--- utils/Statistic/interpolateBadChs.m
+++ utils/Statistic/interpolateBadChs.m
@@ -6,14 +6,21 @@ function trialsECOG = interpolateBadChs(trialsECOG, badCHs, neighbours)
 
     if nargin < 3 || isempty(neighbours)
         % default: for ECoG recording of an 8*8 electrode array
-        [~, neighbours] = mPrepareNeighbours;
+        neighbours = mPrepareNeighbours;
     end
+    
+    neighbch = neighbours.neighbch;
 
     % Replace bad chs by averaging neighbour chs
     for bIndex = 1:numel(badCHs)
         
         for tIndex = 1:length(trialsECOG)
-            chsTemp = neighbours{badCHs(bIndex)};
+            chsTemp = neighbch{badCHs(bIndex)};
+
+            if isempty(chsTemp)
+                warning(['No neighbours specified for channel ', num2str(tIndex), '. Skip']);
+                continue;
+            end
 
             if all(ismember(chsTemp, badCHs))
                 error(['All neighbour channels are bad for channel ', num2str(badCHs(bIndex))]);
--- utils/Statistic/mPrepareNeighbours.m
+++ utils/Statistic/mPrepareNeighbours.m
@@ -1,4 +1,4 @@
-function [neighbours, neighboursInDouble] = mPrepareNeighbours(channels, topoSize)
+function neighbours = mPrepareNeighbours(channels, topoSize)
     narginchk(0, 2);
 
     if nargin < 1
@@ -12,16 +12,14 @@ function [neighbours, neighboursInDouble] = mPrepareNeighbours(channels, topoSiz
     % neighbours
     A0 = reshape(channels, topoSize);
     A = padarray(A0, [1, 1]);
-    neighbours = struct("label", cellfun(@(x) num2str(x), num2cell(channels), 'UniformOutput', false), "neighblabel", cell(1, numel(A0)));
+    neighbours = struct("label", cellfun(@(x) num2str(x), num2cell(channels'), 'UniformOutput', false), "neighblabel", cell(numel(A0), 1));
     for index = 1:numel(A0)
         [row, col] = find(A == A0(index));
         temp = A(row - 1:row + 1, col - 1:col + 1);
         temp(temp == 0 | temp == A0(index)) = [];
-        temp = num2str(temp');
-        neighbours(index).neighblabel = mat2cell(temp, ones(size(temp, 1), 1));
+        neighbours(index).neighbch = temp;
+        neighbours(index).neighblabel = arrayfun(@num2str, temp, "UniformOutput", false);
     end
-
-    neighboursInDouble = arrayfun(@(x) cellfun(@(y) str2double(y), x.neighblabel), neighbours, "UniformOutput", false)';
 
     return;
 end
