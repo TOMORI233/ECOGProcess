83d6da1240248d0617b69bdd169cfd0647f1209e
TOMORI233 <ihatewo@qq.com>
2024-09-26 12:09:34 +0800

    update 2024-09-26 12:09:34 by xhx

--- utils/Statistic/mPrepareNeighbours.m
+++ utils/Statistic/mPrepareNeighbours.m
@@ -1,5 +1,5 @@
-function neighbours = mPrepareNeighbours(channels, topoSize)
-    narginchk(0, 2);
+function neighbours = mPrepareNeighbours(channels, topoSize, opt)
+    narginchk(0, 3);
 
     if nargin < 1
         channels = 1:64;
@@ -9,6 +9,14 @@ function neighbours = mPrepareNeighbours(channels, topoSize)
         topoSize = [8, 8]; % [nx, ny]
     end
 
+    if nargin < 3
+        opt = "surrounding";
+    end
+
+    if ~strcmpi(opt, "surrounding") && ~strcmpi(opt, "orthogonal")
+        error("Invalid syntax");
+    end
+
     % neighbours
     A0 = reshape(channels, topoSize);
     A = padarray(A0, [1, 1]);
@@ -16,7 +24,13 @@ function neighbours = mPrepareNeighbours(channels, topoSize)
     for index = 1:numel(A0)
         [row, col] = find(A == A0(index));
         temp = A(row - 1:row + 1, col - 1:col + 1);
+        
+        if strcmpi(opt, "orthogonal")
+            temp([1, 3, 7, 9]) = 0;
+        end
+
         temp(temp == 0 | temp == A0(index)) = [];
+
         neighbours(index).neighbch = temp;
         neighbours(index).neighblabel = arrayfun(@num2str, temp, "UniformOutput", false);
     end
