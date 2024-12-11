be25003840f1e43c15f5ae2ac4d8121d26b3542d
TOMORI233 <ihatewo@qq.com>
2024-12-11 14:45:43 +0800

    update cbpt

--- utils/Statistic/private/cbptImpl.m
+++ utils/Statistic/private/cbptImpl.m
@@ -1,4 +1,4 @@
-function stat = CBPT(cfg, varargin)
+function stat = cbptImpl(data, cfg)
 % Description: cluster-based permutation test for 2 or more than 2 groups of data
 % NOTICE: If you run into error messages like "too many args input for nearest",
 %         then solution to function name duplication is to add fieldtrip path to
@@ -38,27 +38,54 @@ function stat = CBPT(cfg, varargin)
 %     stat: result of fieldtrip
 %           - prob: prob of cluster-based Monte Carlo permutation test, [nCh, nSample]
 %           - posclusters/negclusters: 1*k struct of information of each cluster
-%           - posclusterslabelmat/negclusterslabelmat: cluster position specified by non-zero values, [nCh, nSample]
+%           - posclusterslabelmat/negclusterslabelmat: cluster position specified by 
+%                                                      non-zero values, [nCh, nSample]
 %           - mask: significant sample position, [nCh, nSample] logical
 %           - stat: the effect at the sample level (t-value or f-value by cfg.statistic), [nCh, nSample]
 
-narginchk(3, inf);
+ft_setPath2Top;
+narginchk(1, 2);
 
-% validate
-[nch, nsample] = checkTrialsData(varargin{:});
-
-data = struct("time"     , cell(length(varargin), 1), ...
-              "label"    , cell(length(varargin), 1), ...
-              "trial"    , cell(length(varargin), 1), ...
-              "trialinfo", cell(length(varargin), 1));
-for index = 1:length(varargin)
-    trialsData = varargin{index};
-    data(index).time = linspace(0, 1, nsample); % normalized time
-    data(index).label = arrayfun(@num2str, (1:nch)', "UniformOutput", false);
-    data(index).trial = cell2mat(cellfun(@(x) permute(x, [3, 1, 2]), trialsData, "UniformOutput", false));
-    data(index).trialinfo = repmat(index, [length(trialsData), 1]);
+if nargin < 2
+    cfg = [];
 end
 
-stat = cbptImpl(data, cfg);
-return;
+cfg_default.method           = 'montecarlo';         % use the Monte Carlo Method to calculate the significance probability
+cfg_default.correctm         = 'cluster';
+cfg_default.correcttail      = 'alpha';              % 'alpha': equivalent to performing a Bonferroni correction for the two tails, i.e., 
+                                                     %          divide alpha by two. Each tail will be tested with alpha = 0.025.
+                                                     % 'prob': multiplying the p-values with a factor of 2
+                                                     
+cfg_default.clusterstatistic = 'maxsum';             % test statistic that will be evaluated under the permutation distribution.
+
+cfg_default.clusteralpha     = 0.05;                 % alpha level of the sample-specific test statistic that will be used for thresholding
+cfg_default.alpha            = 0.05;                 % alpha level of the permutation test
+
+cfg_default.neighbours       = mPrepareNeighbours(); % the neighbours specify for each sensor with which other sensors it can form clusters
+cfg_default.minnbchan        = 0;                    % minimum number of neighborhood channels that is
+                                                     % required for a selected sample to be included
+                                                     % in the clustering algorithm (default=0).
+
+% cfg_default.latency          = [0 1];                % time interval over which the experimental conditions must be compared (in seconds)
+
+cfg_default.numrandomization = 1e3;                  % number of draws from the permutation distribution
+
+if numel(data) == 2
+    cfg_default.statistic    = 'indepsamplesT';      % statistic method to evaluate the effect at the sample level
+    cfg_default.tail         = 0;                    % -1, 1 or 0 (default = 0); one-sided or two-sided test
+    cfg_default.clustertail  = 0;                    % identical to tail option
+else
+    cfg_default.statistic    = 'indepsamplesF';      % statistic method to evaluate the effect at the sample level
+    cfg_default.tail         = 1;                    % -1, 1 or 0 (default = 0); one-sided or two-sided test
+    cfg_default.clustertail  = 1;                    % identical to tail option
+end
+
+cfg = getOrFull(cfg, cfg_default);
+
+cfg.channel = data(1).label;                % cell-array with selected channel labels
+cfg.design  = vertcat(data.trialinfo)';     % design matrix
+cfg.ivar    = 1;                            % number or list with indices indicating the independent variable(s)
+
+temp = mat2cell(reshape(data, [numel(data), 1]), ones(numel(data), 1));
+stat = ft_timelockstatistics(cfg, temp{:});
 end
